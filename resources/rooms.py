e71bf8fd15de32b60d2651df14b0a71768ea64bc
Fede <federicomorella@hotmail.com>
2023-04-26 19:36:59 -0300

    Add endpoint to get room data and remove users from room

--- resources/rooms.py
+++ resources/rooms.py
@@ -49,6 +49,29 @@ class Rooms(MethodView):
             abort(500,message='Failed to create new room')
         
 
+@blp.route('/rooms/<int:room_id>')
+class room_operation(MethodView):
+    @jwt_required()
+    @blp.response(200,PlainRoomSchema)    
+    def get(self,room_id):
+        '''return room details without users'''
+        room=db.get_or_404(RoomModel,room_id)
+        return room
+    
+    @jwt_required()
+    def delete(self,room_id):
+        '''remove current user from the room'''
+        room:RoomModel=db.get_or_404(RoomModel,room_id)
+        user=db.get_or_404(UserModel,get_jwt_identity())
+        res=False
+        if user:
+            res=room.removeUser(user)
+        if res:
+            return {'message':f'current user removed from room {room_id}'},200
+        else:
+            return {'message':f'Failed to remove from room {room_id}'},500
+        
+
 
 
 @blp.route('/rooms/<int:room_id>/contacts')
@@ -68,19 +91,21 @@ class Rooms_id(MethodView):
     @jwt_required()
     @blp.arguments(PlainUserSchema)
     def post(self,user_data,room_id):
-        '''add user to the room.
+        '''add user to the room. Only a user from the room can add new users.
         user={"username":"name"}'''
         room:RoomModel=db.get_or_404(RoomModel,room_id) 
         user:UserModel=db.session.query(UserModel).filter(UserModel.username==user_data.get("username")).first()
-        print('------------user to add:',user,flush=True)
-        if not user:
-            abort(404,message='User not found')
-        
-        if room.addUser(user):
-            return {'message':'user added to the room'}
+        current_user=db.get_or_404(UserModel,get_jwt_identity())
+        if current_user in room.users:
+            if not user:
+                abort(404,message='User not found')
+            
+            if room.addUser(user):
+                return {'message':'user added to the room'}
+            else:
+                abort(500,message='Failed to add user')
         else:
-            abort(500,message='Failed to add user')
+            abort(401,message='Only users from the room can add new users')
         
-   
-        
+      
 
